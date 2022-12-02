b3011004690dc4cb047608ff804954294e6aae48
alfonsovillanueva <alfonso@villanuevagarcia.com>
2022-12-01 21:42:02 -0600

    ok

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-var myHeaders = new Headers();
-myHeaders.append("X-Authorization", "vc7t7o5uopriecledy5mrsd7qb3nml");
-myHeaders.append("Cookie", "rp=api.easybroker.com; source=api.easybroker.com");
-
-var requestOptions = {
+var settings = {
+	url: "https://api.easybroker.com/v1/properties",
 	method: "GET",
+	timeout: 0,
 	headers: {
 		"X-Authorization": "vc7t7o5uopriecledy5mrsd7qb3nml",
 		Cookie: "rp=api.easybroker.com; source=api.easybroker.com",
-		"Access-Control-Allow-Origin": "*",
 	},
-	redirect: "follow",
 };
 
-fetch("https://api.easybroker.com/v1/properties", requestOptions)
-	.then((response) => response.text())
-	.then((result) => console.log(result))
-	.catch((error) => console.log("error", error));
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
